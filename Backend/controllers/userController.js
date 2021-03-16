04100cea6783bfaaf333c2f119ced64978bc7da7
Azim Ansari <azimpanjwar@gmail.com>
2021-03-16 11:49:53 +0530

    model changes and updated

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -85,10 +85,10 @@ module.exports = {
             const userData = User.findOne({_id:mongoose.Types.ObjectId(userId)},{name:1, email:1, status:1,profilePic:1,dob:1})
             userData.exec((err, user)=>{
                 if(!user || err){
-                    return res.status(400).json({message:"User not found "})
+                    return res.status(400).json({message:"User not found 😩"})
                 }
                 var userDetails = {userData:user}
-                return res.status(200).json({message:"User details Here !!", userDetails})
+                return res.status(200).json({message:"User details Here ✌️!!", userDetails})
             })
         } catch (err) {
             res.status(500).json({message:"Server error 🙏"}); 
